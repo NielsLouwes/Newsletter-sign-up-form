13949cb47ca8b70977b1c9ea8ad357f21ddd18d4
Niels Louwes <niels.louwes@and.digital>
2023-06-08 15:29:26 +0200

    added success message

--- src/app/components/SubscribeForm/SubscribeForm.styles.tsx
+++ src/app/components/SubscribeForm/SubscribeForm.styles.tsx
@@ -11,7 +11,7 @@ const MainTitle = styled.h1`
   font-size: 2.5rem;
   margin: 8px;
   margin-bottom: 25px;
-`
+`;
 
 const BodyContainer = styled.div`
   padding: 15px;
@@ -20,7 +20,7 @@ const BodyContainer = styled.div`
 const BottomFormContainer = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const SubscribeButton = styled.button`
   width: 99%;
@@ -29,7 +29,8 @@ const SubscribeButton = styled.button`
   border-radius: 5px;
   padding: 15px;
   font-size: 1rem;
-`
+  cursor: pointer;
+`;
 
 const StyledField = styled(Field)`
   padding: 15px;
@@ -39,28 +40,38 @@ const StyledField = styled(Field)`
   width: 90%;
   font-size: 1rem;
   margin-top: 5px;
-  border-color: ${(props) => (props['aria-invalid'] ? 'hsl(4, 100%, 67%)' : 'initial')};
+  cursor: pointer;
+  border-color: ${(props) =>
+    props['aria-invalid'] ? 'hsl(4, 100%, 67%)' : 'hsl(231, 7%, 60%)'};
 
   ::placeholder {
-    color: ${(props) => (props['aria-invalid'] ? 'hsl(4, 100%, 67%)' : 'initial')};
+    color: ${(props) =>
+      props['aria-invalid'] ? 'hsl(4, 100%, 67%)' : 'hsl(231, 7%, 60%)'};
   }
 `;
 
 const EmailTitle = styled.span`
   font-size: 0.8rem;
   padding: 2px;
-`
+`;
 
 const StyledError = styled(ErrorMessage)`
   font-size: 0.8rem;
   padding: 2px;
   color: hsl(4, 100%, 67%);
-`
+`;
 
 const EmailContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
+
+const SuccessForm = styled.div`
+  width: 375px;
+  background: white;
+  height: 750px;
+  font-family: 'Roboto';
+`;
 
 export const SubscribeFormStyled = {
   FormContainer,
@@ -71,5 +82,6 @@ export const SubscribeFormStyled = {
   EmailTitle,
   StyledError,
   EmailContainer,
-  MainTitle
+  MainTitle,
+  SuccessForm
 };
--- src/app/components/SubscribeForm/SubscribeForm.tsx
+++ src/app/components/SubscribeForm/SubscribeForm.tsx
@@ -25,48 +25,62 @@ const renderedListItems = () => {
 export function SubscribeForm() {
   return (
     <SubscribeFormStyled.FormContainer>
-      <img src={logoMobile} alt="" />
-      <SubscribeFormStyled.BodyContainer>
-        <SubscribeFormStyled.MainTitle>Stay updated!</SubscribeFormStyled.MainTitle>
-        <body style={{marginBottom: '25px'}}>Join 60,000+ product managers receiving monthly updates on:</body>
-        <body>{renderedListItems()}</body>
-        <Formik
-          initialValues={{ email: '' }}
-          validationSchema={validationSchema}
-          onSubmit={async (values) => {
-            // Form submission logic goes here
-            console.log(values);
-          }}
-        >
-          {({ isSubmitting, errors }) => (
-            <Form>
-              <SubscribeFormStyled.EmailContainer>
-                <SubscribeFormStyled.EmailTitle>
-                  Email Adress
-                </SubscribeFormStyled.EmailTitle>
-                <SubscribeFormStyled.StyledError
-                  name="email"
-                  component="span"
-                />
-              </SubscribeFormStyled.EmailContainer>
-              <SubscribeFormStyled.BottomFormContainer>
-                <SubscribeFormStyled.StyledField
-                  aria-invalid={errors.email ? true : false}
-                  type="email"
-                  name="email"
-                  placeholder="email@company.com"
-                />
-              </SubscribeFormStyled.BottomFormContainer>
-              <SubscribeFormStyled.SubscribeButton
-                type="submit"
-                disabled={isSubmitting}
-              >
-                Subscribe to monthly newsletter
-              </SubscribeFormStyled.SubscribeButton>
-            </Form>
-          )}
-        </Formik>
-      </SubscribeFormStyled.BodyContainer>
+      <Formik
+        initialValues={{ email: '' }}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting, setStatus }) => {
+          // Fake a network request
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          // Form submission logic goes here
+         setStatus({ success: true, email: values.email });
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, errors, status }) =>
+          status && status.success ? (
+            <SubscribeFormStyled.SuccessForm>
+              <img src={Icon} alt="" height="50px" width="50px" />
+              <SubscribeFormStyled.MainTitle>Thank you for subscribing!</SubscribeFormStyled.MainTitle>
+              <body>A confirmation email has been sent to {status.email} Please open it and click the button inside to confirm your subscription</body>
+              </SubscribeFormStyled.SuccessForm>
+          ) : (
+            <SubscribeFormStyled.BodyContainer>
+                <SubscribeFormStyled.MainTitle>
+                  Stay updated!
+                </SubscribeFormStyled.MainTitle>
+                <body style={{ marginBottom: '25px' }}>
+                  Join 60,000+ product managers receiving monthly updates on:
+                </body>
+                <body>{renderedListItems()}</body>
+                <Form>
+                  <SubscribeFormStyled.EmailContainer>
+                    <SubscribeFormStyled.EmailTitle>
+                      Email Adress
+                    </SubscribeFormStyled.EmailTitle>
+                    <SubscribeFormStyled.StyledError
+                      name="email"
+                      component="span"
+                    />
+                  </SubscribeFormStyled.EmailContainer>
+                  <SubscribeFormStyled.BottomFormContainer>
+                    <SubscribeFormStyled.StyledField
+                      aria-invalid={errors.email ? true : false}
+                      type="email"
+                      name="email"
+                      placeholder="email@company.com"
+                    />
+                  </SubscribeFormStyled.BottomFormContainer>
+                  <SubscribeFormStyled.SubscribeButton
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Subscribe to monthly newsletter
+                  </SubscribeFormStyled.SubscribeButton>
+                </Form>
+              </SubscribeFormStyled.BodyContainer>
+          )
+        }
+      </Formik>
     </SubscribeFormStyled.FormContainer>
   );
 }
